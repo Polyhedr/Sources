43c6fbedb2048e20307d7adda1ffcd4af7e1dec0
U-CORP\pierreper <pierreper@FRPA1-1591.corp.idemia.com>
2025-04-22 14:48:55 +0200

    update

--- generate_Rule_cards.py
+++ generate_Rule_cards.py
@@ -69,7 +69,9 @@ def draw_rounded_rectangle(image, top_left=None, bottom_right=None, color=(255,2
     return image
 
 def main(dpi, padding, output_folder):
-    for rule_path in sorted(glob.glob(os.path.join("rules","*","*",'rule.tex'))):
+    rules = sorted(glob.glob(os.path.join("rules","*","*",'rule.tex')))
+    print(rules)
+    for rule_path in rules:
         game_path = Path(rule_path).parent
 
         # get the pdf with lualatex
